a83ad952b1d68d66a8f7dad9bd89180f9bab3f93
Gabriel Riveros <velo02.gr@gmail.com>
2024-05-11 17:31:55 -0400

    Actualizar cambiar video de mobile a tablet con javascript

--- js/app.js
+++ js/app.js
@@ -11,3 +11,15 @@ const handleLabel = () => {
 
 // Evento click => llamar a handleLabel
 navLabel.addEventListener('click', handleLabel);
+
+window.addEventListener('resize', function(){
+    let windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const video = document.querySelector('.itro__video');
+    if(windowWidth < 768){
+        video.setAttribute('src', './assets/video-mobile.mp4');
+    }else{
+        video.setAttribute('src', './assets/video-tablet.mp4');
+    }
+});
+
+window.dispatchEvent(new Event('resize'));
